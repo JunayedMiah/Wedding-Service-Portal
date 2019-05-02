128d509d802ae40b7f056b9591893338b48835ca
khshohag <48988224+khshohag@users.noreply.github.com>
2019-05-02 19:47:37 +0600

    Add files via upload

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,7 @@ gulp.task('sass', function(){
 
 // Move JS Files to SRC
 gulp.task('js', function(){
-  return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/tether/dist/js/tether.min.js'])
+  return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/popper.min.js'])
     .pipe(gulp.dest("src/js"))
     .pipe(browserSync.stream());
 });
